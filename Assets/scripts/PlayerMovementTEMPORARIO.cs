310ce7302436c45eed0672024b8afff4561a5a1e
sousa125 <69760193+sousa125@users.noreply.github.com>
2021-08-21 12:09:47 -0300

    Adicionar powerup de velocidade

--- Assets/scripts/PlayerMovementTEMPORARIO.cs
+++ Assets/scripts/PlayerMovementTEMPORARIO.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class PlayerMovementTEMPORARIO : MonoBehaviour
 {
-    
+    public float speed = 10.0f;
+
     // Script rapido só pra testes
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput =  Input.GetAxis("Vertical");
         
-        transform.position += new Vector3(horizontalInput, verticalInput, transform.position.z) * Time.deltaTime * 10.0f;
+        transform.position += new Vector3(horizontalInput, verticalInput, transform.position.z) * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("SpeedPower"))
+        {
+            speed = 20.0f;
+            Destroy(other.gameObject);
+        }
     }
 }
