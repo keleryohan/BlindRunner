776cdab4e5fb61d7071e73124e07642a01d07151
keleryohan <keler.yohan@hotmail.com>
2021-08-24 14:56:09 -0300

    ajustes e poder de speedup

--- Assets/Scripts/Powers.cs
+++ Assets/Scripts/Powers.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Powers : MonoBehaviour
 {
+    enum PlayerSpeed
+    {
+        faster = 15,
+        normal = 10,
+        slowed = 1,
+        stopped = 0
+    } 
+
     [SerializeField] GameObject _oponentPlayer;
     [SerializeField] KeyCode _power1;
     [SerializeField] KeyCode _power2;
+    [SerializeField] KeyCode _power3;
 
     [SerializeField] float _powerCoolDown = 5.0f;
 
@@ -29,6 +38,12 @@ public class Powers : MonoBehaviour
                 string mensagem2 = "Power 2 ativado no " + this.gameObject.tag + " contra o " + _oponentPlayer.gameObject.tag;
                 Debug.Log(mensagem2);
             }
+            else if (Input.GetKey(_power3))
+            {
+                StartCoroutine(SpeedUp());
+                string mensagem3 = "Power 3 ativado no " + this.gameObject.tag + " contra o " + _oponentPlayer.gameObject.tag;
+                Debug.Log(mensagem3);
+            }
         }
     }
 
@@ -36,12 +51,12 @@ public class Powers : MonoBehaviour
     {
         _isPowerActive = true;
         Color spritecolor = _oponentPlayer.GetComponent<SpriteRenderer>().color;
-        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = 1.0f;
+        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = (float)PlayerSpeed.slowed;
         _oponentPlayer.GetComponent<SpriteRenderer>().color = Color.red;
 
-        yield return new WaitForSeconds(_powerCoolDown); 
+        yield return new WaitForSeconds(_powerCoolDown);
 
-        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = 10.0f;
+        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = (float)PlayerSpeed.normal;
         _oponentPlayer.GetComponent<SpriteRenderer>().color = spritecolor;
         _isPowerActive = false;
     }
@@ -50,12 +65,26 @@ public class Powers : MonoBehaviour
     {
         _isPowerActive = true;
         Color spritecolor = _oponentPlayer.GetComponent<SpriteRenderer>().color;
-        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = 0f;
+        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = (float)PlayerSpeed.stopped;
         _oponentPlayer.GetComponent<SpriteRenderer>().color = Color.blue;
 
-        yield return new WaitForSeconds(_powerCoolDown); 
+        yield return new WaitForSeconds(_powerCoolDown);
 
-        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = 10.0f;
+        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = (float)PlayerSpeed.normal;
+        _oponentPlayer.GetComponent<SpriteRenderer>().color = spritecolor;
+        _isPowerActive = false;
+    }
+
+    private IEnumerator SpeedUp()
+    {
+        _isPowerActive = true;
+        Color spritecolor = _oponentPlayer.GetComponent<SpriteRenderer>().color;
+        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = (float)PlayerSpeed.faster;
+        _oponentPlayer.GetComponent<SpriteRenderer>().color = Color.magenta;
+
+        yield return new WaitForSeconds(_powerCoolDown);
+
+        _oponentPlayer.GetComponent<PlayerMovementTEMPORARIO>().speed = (float)PlayerSpeed.normal;
         _oponentPlayer.GetComponent<SpriteRenderer>().color = spritecolor;
         _isPowerActive = false;
     }
