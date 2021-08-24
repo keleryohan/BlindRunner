fe7a8ed5dfcf9897083085c864ca70f2c15e2d6a
Elexandro Torres <elexandro.torres@gmail.com>
2021-08-23 23:57:23 -0300

    Junção das funções, corrigido erro

--- Assets/scripts/RespawnBehavior.cs
+++ Assets/scripts/RespawnBehavior.cs
@@ -4,20 +4,9 @@ using UnityEngine;
 
 public class RespawnBehavior : MonoBehaviour
 {
-    //todo: definir spawn points ao início da fase
+    
     Vector2 respawnPoint = Vector2.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void PlayerCollidedWall(GameObject player)
     {
