fc3f0d300251d11696a7fd16e93dd2ccefcd7c4d
Elexandro Torres <elexandro.torres@gmail.com>
2021-08-24 15:52:01 -0300

    Finalização da fase e limitação a 1 de uso dos powers

--- Assets/Scripts/FinishBehavior.cs
+++ Assets/Scripts/FinishBehavior.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FinishBehavior : MonoBehaviour
 {
@@ -12,5 +13,12 @@ public class FinishBehavior : MonoBehaviour
         string winWorlds = col.gameObject.tag + " Venceu!!!";
         _victoryText.text = winWorlds;
         Debug.Log(winWorlds);
+        StartCoroutine(RestartGame());
+    }
+
+    private IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
