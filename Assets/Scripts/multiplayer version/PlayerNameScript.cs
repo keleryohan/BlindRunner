07db04d527712816f16fd882ee5e9bc78d16c21d
keleryohan <keler.yohan@hotmail.com>
2021-09-10 22:49:02 -0300

    cena inicial offline, input para nome dos jogadores

---
+++ Assets/Scripts/multiplayer version/PlayerNameScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerNameScript : NetworkBehaviour
+{
+    public TextMesh playerNameText;
+
+    [SyncVar(hook = nameof(OnNameChanged))]
+    public string playerName;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        string name = GameObject.Find("SceneManager").GetComponent<SceneManagerMP>().auxPlayerName;
+        if (name == null || name == "")
+        {
+            name = "Player" + Random.Range(100, 999);
+        }
+        CmdSetupPlayer(name);
+    }
+
+    [Command]
+    public void CmdSetupPlayer(string _name)
+    {
+        playerName = _name;
+    }
+
+    void OnNameChanged(string _Old, string _New)
+    {
+        playerNameText.text = playerName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
